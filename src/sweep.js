d6ebac2cb491a4b695f5b12186c2383eef9eab8e
Carlo Trönnberg <carlo.tronnberg@hu.ibm.com>
2021-11-26 03:47:06 +0100

    test: 🍅 #, US #, Scenario # 🔴 [Red]\n\nDummy files

---
+++ src/sweep.js
@@ -0,0 +1,5 @@
+function sweep(a, b) {
+  return a + b;
+}
+module.exports = sweep;
+
---
+++ test/sweep.test.js
@@ -0,0 +1,21 @@
+const sweep = require('../src/sweep');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sweep(1, 2)).toBe(3);
+});
+
+describe('UAT description (what the user would accept)', () => {
+  describe('Scenario #1 - Adding numbers', () => {
+    test.each`
+      a    | b    | expected
+      ${1} | ${2} | ${3}
+      ${3} | ${2} | ${5}
+    `(
+      'GIVEN numbers $a and $b WHEN adding them THEN we should get $expected',
+      ({ a, b, expected }) => {
+        expect(sweep(a, b)).toEqual(expected);
+      }
+    );
+  });
+});
+
